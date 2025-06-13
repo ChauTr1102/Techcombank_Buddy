9d3faf8a900ace42ed3270b377fc6add43e898e1
Ngô Đức Mạnh <ducmanh4009@gmail.com>
2025-06-13 16:58:18 +0700

    dfg
    df

--- api/services/routing_agent.py
+++ api/services/routing_agent.py
@@ -7,10 +7,10 @@ class RoutingAgent:
         self.model_llm = ChatGoogleGenerativeAI(temperature=TEMPERATURE, model="gemini-2.5-flash-preview-05-20",
                                                 api_key=apikey)
 
-    def decision(self, history, user_input):
+    def route_decision(self, history, user_input):
         pass
 
-    def prompt_routing(self, user_input, history):
+    def route_prompt_routing(self, user_input, history):
         llm_prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", "**Instruction 1:**\n {prompt_routing}\n"),
