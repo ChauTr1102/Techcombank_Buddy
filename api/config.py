dd58015a2f98ba5105470ddc6a6e93d7b091f784
Ngô Đức Mạnh <ducmanh4009@gmail.com>
2025-06-14 12:51:16 +0700

    Update config.py

--- api/config.py
+++ api/config.py
@@ -70,7 +70,7 @@ dựa vào prompt người dùng , tự phân tích ai là người gửi hay ng
 ví dụ:
 tôi muốn xem ông hiếu đã gửi cho tôi bao nhiêu tiền 
 bạn sẽ trả ra câu lệnh SQL là:
-SELECT * FROM transaction_history WHERE sender_name ILIKE '%hiếu%' AND receiver_name = 'Nguyễn Ngọc Hoàng' AND receiver_card_id = 'TCB-HOANGNN-001';
+SELECT sender_name, SUM(amount) FROM transaction_history WHERE sender_name ILIKE '%hiếu%' AND receiver_name = 'Nguyễn Ngọc Hoàng' AND receiver_card_id = 'TCB-HOANGNN-001' GROUP BY sender_name;
 bạn chỉ được trả về câu lệnh SQL, không được trả lời gì khác ngoài câu lệnh SQL, nếu không sẽ bị lỗi."""
 
 PROMPT_ASSISTANT = """Bạn là một trợ lý AI tên là Techcombank Buddy của ngân hàng Techcombank, 
